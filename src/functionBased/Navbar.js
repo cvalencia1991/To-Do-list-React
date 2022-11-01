d2d65c7ff78642c7f3ba07d1c394a4ab07cd5e92
cvalencia1991 <cesar4a6z@gmail.com>
2022-11-01 15:21:39 -0500

    Add the funcitonailty of Hamburger Menu

--- src/functionBased/Navbar.js
+++ src/functionBased/Navbar.js
@@ -1,25 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import { NavLink } from "react-router-dom"
 
-const links = [
-    {
-      id: 1,
-      path: "/",
-      text: "Home",
-    },
-    {
-      id: 2,
-      path: "/about",
-      text: "About",
-    },
-  ]
 const Navbar = () => {
+
+    const links = [
+        {
+          id: 1,
+          path: "/",
+          text: "Home",
+        },
+        {
+          id: 2,
+          path: "/about",
+          text: "About",
+        },
+      ]
+    const handleToggle = () => {
+        setNavbarOpen(!navbarOpen)
+      }
+      const closeMenu = () => {
+        setNavbarOpen(false)
+      }
+    const [navbarOpen, setNavbarOpen] = useState(false)
   return (
     <nav className="navBar">
-    <ul>
+   <button onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</button>
+    <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
       {links.map(link => {
         return (<li key={link.id}>
-                <NavLink to={link.path} activeClassName="active-link" exact>
+                <NavLink 
+                to={link.path} 
+                activeClassName="active-link" 
+                onClick={() => closeMenu()}
+                exact>
                     {link.text}
                     </NavLink>
             </li>
